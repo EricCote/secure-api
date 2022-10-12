e2064694f275f02c00bd28ba46e2376d90795836
Eric Cote <eric@coteexpert.com>
2022-10-12 10:48:53 -0400

    fix stuff

--- app/config/keycloak-config.js
+++ app/config/keycloak-config.js
@@ -4,7 +4,7 @@ const Keycloak = require('keycloak-connect');
 let _keycloak;
 
 var keycloakConfig = {
-    serverUrl: "https://keycloak2.reactacademy.live/",
+    serverUrl: "https://keycloak3.reactacademy.live/",
     realm: "react-courses",
     clientId: "august-course",
     realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlgE18nhFEu2S2b4O5GXq4Wk1d1YxSnhmFHwt9F77gCgqEZkeNNWSghBYwybyNuisIDsFwccJzaOQjUj9Trq51S0u6EtKtXlhNd5R8GjIkj7aUJ30bsruGwISt8EAbNhxdDq8ODNuDpM0ONENDFuPgivLHcBAFSeap4MviW2qmPZv4DXdUVUbK/1/zM2VuURM+8xzEa92BbZhHZJM49r4nIi4aprtXIW+KDs301P35WY7CXIO4tebaX2voPt7jIFhVCOaoQyTeM+eO6Co0IJggQqOj6M/2vfW7ipBxRHmw76fOjlpm8SMj7eMtHKyJxGpskliF5VG01it69sKm+7XgwIDAQAB",
