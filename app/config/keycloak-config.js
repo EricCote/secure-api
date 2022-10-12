5d7dabf67e85df76756cfa33def22ea0bd1b4d63
Eric Cote <eric@coteexpert.com>
2022-10-12 09:33:59 -0400

    change key

--- app/config/keycloak-config.js
+++ app/config/keycloak-config.js
@@ -7,7 +7,7 @@ var keycloakConfig = {
     serverUrl: "https://keycloak2.reactacademy.live/",
     realm: "react-courses",
     clientId: "august-course",
-    realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApBAvNIr2JlndzYxCS4/zbr2CEdKtkpfpo3N/6I/150t7ZrWSdDJl0Jr7Bb8215npCG/FzuiP5mLnsslPkP7JUbALJtEduNrQ0mCyrUeLdXoEtuXeLI0rqxsSRxioZED98uA71N1UrsF6I/a58U+cFJqeZ92+zF/a2Ur3MltoT193la0XvxkjFf07u7O92a1EqBCfgwSw1dGpPtK4NMHZCgezKGQXsyTewHapZxz5JDc2gDKOBU9PNOvg1HOCZiEflOCBnceVbEwMRUxtbh0H/3n/Y8B4V4stQhFlUgCKB7p5MGtMPYp5aGVjptCFNVbLse/2qxSlBt2ZAAuE7oIKFwIDAQAB",
+    realmPublicKey: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlgE18nhFEu2S2b4O5GXq4Wk1d1YxSnhmFHwt9F77gCgqEZkeNNWSghBYwybyNuisIDsFwccJzaOQjUj9Trq51S0u6EtKtXlhNd5R8GjIkj7aUJ30bsruGwISt8EAbNhxdDq8ODNuDpM0ONENDFuPgivLHcBAFSeap4MviW2qmPZv4DXdUVUbK/1/zM2VuURM+8xzEa92BbZhHZJM49r4nIi4aprtXIW+KDs301P35WY7CXIO4tebaX2voPt7jIFhVCOaoQyTeM+eO6Co0IJggQqOj6M/2vfW7ipBxRHmw76fOjlpm8SMj7eMtHKyJxGpskliF5VG01it69sKm+7XgwIDAQAB",
     bearerOnly: true,
    
 };
